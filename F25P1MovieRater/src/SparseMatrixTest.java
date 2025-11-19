da412bfa7c81522af1b598951cb3c05676f03344
Kevin <kevindong1664@gmail.com>
2025-11-18 23:21:25 -0500

    Update SparseMatrixTest.java

--- F25P1MovieRater/src/SparseMatrixTest.java
+++ F25P1MovieRater/src/SparseMatrixTest.java
@@ -8,10 +8,10 @@ public class SparseMatrixTest extends student.TestCase {
 
 
     public void testInsert() {
-        test.insert(8, 0, 0);
-        test.insert(9, 2, 0);
-        test.insert(1, 0, 1);
-        test.insert(9, 4, 11);
+        test.insertScore(8, 0, 0);
+        test.insertScore(9, 2, 0);
+        test.insertScore(1, 0, 1);
+        test.insertScore(9, 4, 11);
 
         assertFalse(test.search(0, 0, 0));
         assertTrue(test.search(8, 0, 0));
@@ -22,10 +22,10 @@ public class SparseMatrixTest extends student.TestCase {
 
 
     public void testDelete() {
-        test.insert(8, 0, 0);
-        test.insert(9, 2, 0);
-        test.insert(1, 0, 1);
-        test.insert(9, 4, 11);
+        test.insertScore(8, 0, 0);
+        test.insertScore(9, 2, 0);
+        test.insertScore(1, 0, 1);
+        test.insertScore(9, 4, 11);
 
         assertFalse(test.search(0, 0, 0));
         assertTrue(test.search(8, 0, 0));
